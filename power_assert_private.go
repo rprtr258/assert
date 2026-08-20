4beea9e452fbc38a3ef108df1a470a023961d97e
Poletansky Viktor <poletansky@gmail.com>
2026-08-20 14:18:54 +0300

    implement PR#4

--- power_assert_private.go
+++ power_assert_private.go
@@ -273,7 +273,7 @@ func getModuleDir() (_dir, _pkgDir string, _err error) {
 	}
 	// os.Getwd does not cut it, since tests are being run from a temp dir using temporary executable
 	// so we have to do caller getting trickery and extract module path the hard way
-	_, file, _, ok := runtime.Caller(4) // Assert/Require -> fuse -> run -> getModuleDir
+	_, file, _, ok := runtime.Caller(3) // TestMain -> Fuse -> run -> getModuleDir
 	if !ok {
 		return "", "", errGetCaller
 	}
@@ -302,17 +302,17 @@ func getModuleDir() (_dir, _pkgDir string, _err error) {
 }
 
 //nolint:maintidx
-func run() error {
+func run() (int, error) {
 	moduleDir, pkgRelDir, err := getModuleDir()
 	if err != nil {
-		return fmt.Errorf("get module dir: %w", err)
+		return 0, fmt.Errorf("get module dir: %w", err)
 	}
 
 	debugf("module dir %s, package dir %s", moduleDir, pkgRelDir)
 
 	tmpDir, err := os.MkdirTemp("", "assert.*")
 	if err != nil {
-		return fmt.Errorf("create temp dir: %w", err)
+		return 0, fmt.Errorf("create temp dir: %w", err)
 	}
 
 	if !debug {
@@ -360,7 +360,7 @@ func run() error {
 
 		return linkFile(path, dst)
 	}); err != nil {
-		return fmt.Errorf("copy project to temp dir: %w", err)
+		return 0, fmt.Errorf("copy project to temp dir: %w", err)
 	}
 
 	testfiles := []string{}
@@ -374,23 +374,23 @@ func run() error {
 
 		return nil
 	}); err != nil {
-		return fmt.Errorf("collect test files: %w", err)
+		return 0, fmt.Errorf("collect test files: %w", err)
 	}
 
 	for _, fileRelPath := range testfiles {
 		ff, err := os.Open(fileRelPath)
 		if err != nil {
-			return fmt.Errorf("open test file %s: %w", fileRelPath, err)
+			return 0, fmt.Errorf("open test file %s: %w", fileRelPath, err)
 		}
 
 		stat, err := ff.Stat()
 		if err != nil {
-			return fmt.Errorf("stat test file %s: %w", fileRelPath, err)
+			return 0, fmt.Errorf("stat test file %s: %w", fileRelPath, err)
 		}
 
 		f, err := io.ReadAll(ff)
 		if err != nil {
-			return fmt.Errorf("read test file %s: %w", fileRelPath, err)
+			return 0, fmt.Errorf("read test file %s: %w", fileRelPath, err)
 		}
 
 		ff.Close()
@@ -402,7 +402,7 @@ func run() error {
 
 		root, err := parser.ParseFile(token.NewFileSet(), fileRelPath, f, 0)
 		if err != nil {
-			return fmt.Errorf("parse test file %s: %w", fileRelPath, err)
+			return 0, fmt.Errorf("parse test file %s: %w", fileRelPath, err)
 		}
 
 		found := false
@@ -529,7 +529,7 @@ func run() error {
 		debugf("rewriting %s", fileRelPath)
 
 		if err := os.WriteFile(fileRelPath, []byte(sprintCode(root)), stat.Mode()); err != nil {
-			return fmt.Errorf("write rewritten file %s: %w", fileRelPath, err)
+			return 0, fmt.Errorf("write rewritten file %s: %w", fileRelPath, err)
 		}
 	}
 
@@ -547,10 +547,14 @@ func run() error {
 
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
-		return fmt.Errorf("run tests: %w", err)
+		if exitErr, ok := errors.AsType[*exec.ExitError](err); ok {
+			return exitErr.ExitCode(), nil
+		}
+
+		return 1, fmt.Errorf("run tests: %w", err)
 	}
 
-	return nil
+	return 0, nil
 }
 
 // linkFile hard-links src into dst; when a hard link is impossible (cross-
